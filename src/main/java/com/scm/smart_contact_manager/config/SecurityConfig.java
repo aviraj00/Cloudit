a2ddb12b52a0d2d7e96f5e982ee44e07d51a2ebc
Aviraj lavate <101280208+aviraj00@users.noreply.github.com>
2024-12-23 18:36:56 +0530

    created dahboard and profile

--- src/main/java/com/scm/smart_contact_manager/config/SecurityConfig.java
+++ src/main/java/com/scm/smart_contact_manager/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.scm.smart_contact_manager.config;
 
 
-import com.scm.smart_contact_manager.services.implimentation.SecurityCustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.scm.smart_contact_manager.services.implimentation.SecurityCustomUserDetails;
 
 @Configuration
 public class SecurityConfig {
